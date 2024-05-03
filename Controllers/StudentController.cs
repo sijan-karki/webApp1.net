3228ab6aa8fb88a35853096f5956f657694fa685
SijanUnleashed <112467487+sijan-karki@users.noreply.github.com>
2024-05-03 13:12:47 +0545

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebApp1BySijan.Models;
+
+namespace WebApp1BySijan
+{
+    public class StudentController : Controller
+    {
+        Student s = new Student()
+        {
+            StdID = 12,
+            Name = "Sagar Ale",
+            Address = "123 sivaylayaTol",
+            Faculty = "Computer Science"
+        };
+        public IActionResult MyRazorPage()
+        {
+            return View(s);
+        }
+        public IActionResult CreateStudentRecord()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudentRecord(Student s)
+        {
+            if (ModelState.IsValid)
+                return View("StudentDetail", s);
+            else
+                return View(s);
+        }
+
+        public IActionResult StudentDetail()
+        {
+            return View(s);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApp1BySijan.Models
+{
+    public class Student
+    {
+        [Required]
+        [Range(0,299, ErrorMessage = "Student ID must be between 0 and 299")]
+        public int StdID {  get; set; }
+
+        [Required]
+        [StringLength(20,ErrorMessage ="Name length cannot be more than 10 ")]
+        public String Name {  get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Address length cannot be more than 20 ")]
+        public String Address {  get; set; }
+
+        [Required(ErrorMessage ="Faculty is required")]
+        public String Faculty {  get; set; }
+    }
+}
